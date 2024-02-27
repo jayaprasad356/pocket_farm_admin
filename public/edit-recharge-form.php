4bf09d9ee0ab9f5af68e26aa014387607027aba7
jayaprasad356 <jayaprasad356@gmail.com>
2024-02-28 03:25:23 +0530

    recharge

--- public/edit-recharge-form.php
+++ public/edit-recharge-form.php
@@ -87,10 +87,8 @@ $result = $db->getResult();
                             </div>   
                         </div>
                         <br>
-
-                    </div>
-                    <div class="row">
-                    <div class="form-group col-md-4">
+                        <div class="row">
+                        <div class="form-group col-md-8">
                                     <label class="control-label">Status</label><i class="text-danger asterik">*</i><br>
                                     <div id="status" class="btn-group">
                                         <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
@@ -100,10 +98,12 @@ $result = $db->getResult();
                                             <input type="radio" name="status" value="1" <?= ($res[0]['status'] == 1) ? 'checked' : ''; ?>> Verified
                                         </label>
                                         <label class="btn btn-danger" data-toggle-class="btn-default" data-toggle-passive-class="btn-default">
-                                            <input type="radio" name="status" value="2" <?= ($res[0]['status'] == 2) ? 'checked' : ''; ?>> Blocked
+                                            <input type="radio" name="status" value="2" <?= ($res[0]['status'] == 2) ? 'checked' : ''; ?>> Cancelled
                                         </label>
                                     </div>
                                 </div>
+                        </div>
+
                     </div>
 
                     <div class="box-footer">
