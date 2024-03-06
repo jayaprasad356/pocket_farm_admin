4ddbe61e5a8314e54aadd6081ba01a09067e6d5f
ashok3221 <ashokkumarraja77712@gmail.com>
2024-03-06 19:37:55 +0530

    export-withdrawals.php

--- export-withdrawals.php
+++ export-withdrawals.php
@@ -5,7 +5,12 @@ $db->connect();
 date_default_timezone_set('Asia/Kolkata');
 $currentdate = date('Y-m-d');
 
-$sql = "SELECT u.name, u.mobile,u.email,CONCAT(',', u.account_num, ',') AS account_num, u.ifsc, u.bank, u.branch
+$sql = "SELECT 
+u.name AS `Beneficiary Name (Mandatory) Full name of the customer - eg: Bruce Wayne`, 
+CONCAT('', u.account_num, '') AS `Beneficiary Account number (Mandatory) Beneficiary Account number to which the money should be transferred`, 
+u.ifsc AS `IFSC code (Mandatory) IFSC code of beneficary's bank. eg:KKBK0000958`, 
+w.amount AS `Amount (Mandatory) Amount that needs to be transfered. Eg: 100.00`, 
+NULL AS `Description / Purpose (Optional) For Internal Reference eg: For salary`
 FROM users u JOIN  withdrawals w ON u.id = w.user_id";
 	$db->sql($sql);
 	$developer_records = $db->getResult();
