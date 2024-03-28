1bb234720acb79cb1996ec0880133cbd22d7c773
Akraja712 <ashokkumar7hitter@gmail.com>
2024-03-28 19:55:48 +0530

    markets_list.php

---
+++ api/markets_list.php
@@ -0,0 +1,54 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User ID is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+
+$sql = "SELECT * FROM users WHERE id = $user_id ";
+$db->sql($sql);
+$user = $db->getResult();
+
+if (empty($user)) {
+    $response['success'] = false;
+    $response['message'] = "User not found";
+    print_r(json_encode($response));
+    return false;
+}
+
+
+$sql = "SELECT * FROM markets";
+$db->sql($sql);
+$res= $db->getResult();
+$num = $db->numRows($res);
+
+if ($num >= 1){
+ 
+    $response['success'] = true;
+    $response['message'] = "Markets Listed Successfully";
+    $response['data'] = $res;
+    print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Markets Not found";
+    print_r(json_encode($response));
+
+}
+
+
